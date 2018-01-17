a1b62387141d0844deba288e2168f6fdd90951a3
jospeh.walker.tmp@gmail.com <joseph.walker.tmp@gmail.com>
2018-01-17 13:48:56 -0500

    Added definition for winner tree

--- Tree/Tree.h
+++ Tree/Tree.h
@@ -4,18 +4,21 @@
 #include "../Map/Map.h"
 
 /**
- *
+ * This struct defines an individual node in the winner tree
  */
 typedef struct WinnerTreeNode WinnerTreeNode;
 
 /**
+ * This structure defines the node for the winner tree. Winner trees will be passed around as pointers
+ * to the current root node. The root node does not necessarily have to be the empty map node, but the
+ * node for the current state of the game.
  *
- *
- * @param map:
- * @param children:
- * @param player:
- * @param wins:
- * @param losses:
+ * @param map: The map that represents the current state of the game.
+ * @param children: A list of possible states to follow this one.
+ * @param player: The player that is being considered for this tree.
+ * @param wins: The number of winning paths from this state.
+ * @param losses: The number of loss path from this state.
+ * @param draws: The number of draw paths from this state.
  */
 struct WinnerTreeNode
 {
@@ -24,6 +27,7 @@ struct WinnerTreeNode
     TileState player;
     int wins;
     int losses;
+    int draw;
 };
 
 
